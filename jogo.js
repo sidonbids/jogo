7ed5d6d49c4a2cdd10682e6dbb90380725c6d874
sidonbids <122887513+sidonbids@users.noreply.github.com>
2023-04-06 10:53:38 -0300

    Add files via upload

---
+++ jogo.js
@@ -0,0 +1,8 @@
+const cartas = [
+    { id: 1, nome: 'carta1' },
+    { id: 2, nome: 'carta2' },
+    { id: 3, nome: 'carta3' },
+    { id: 4, nome: 'carta4' },
+    { id: 5, nome: 'carta5' },
+    { id: 6, nome: 'carta6' },
+  
